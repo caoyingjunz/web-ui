ff05ed3e360225094e6ef9b3d598cd22e9c9b1f0
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-01 23:30:52 +0800

    complete setup and ref

---
+++ src/components/vue3-002.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <h1>姓名: {{name}}</h1>
+    <h1>年龄: {{age}} </h1>
+    <h1>公司: {{company}}</h1>
+    <h1>产品: {{ obj.tabao}}</h1>
+    <h1 v-for="(item, index) in arr" :key="index">{{item.xiaomi}} </h1>
+</div>
+<hr>
+
+<button @click="btn"> 点击更新 </button>
+
+</template>
+
+<script>
+
+import {ref} from 'vue'
+
+export default({
+  setup() {
+      const name = ref('曹英俊')
+      const age = 18
+      const company = '九州云'
+
+      const obj = ref({tabao:'边缘云计算', tianmao:'OpenStack'})
+
+      const arr = ref([{
+          xiaomi: '小米'
+      }])
+
+      function btn() {
+            console.log("btn")
+            name.value = '英俊'
+            obj.value.tabao= "kubernetes"
+            arr.value[0].xiaomi = "小米2"
+      }
+
+      return {name, age, company, obj, btn,arr}
+  }
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus' // 引入element-plus
+import 'element-plus/theme-chalk/index.css' // 引入element-plus的样式
+
+
+createApp(App)
+    .use(ElementPlus, { zIndex: 3000, size: 'small' })
+    .mount('#app')
