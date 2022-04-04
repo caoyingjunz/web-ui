3a994a156ed89df1e5481ce0a6f06cf7adc1ef13
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-05 00:28:46 +0800

    Add login and register API completed (#2)

---
+++ api/request.js
@@ -0,0 +1,38 @@
+// axios 请求
+
+import instance from './hanlder.js'
+
+const request = class{
+    constructor(url, arg) {
+        this.url = url
+        this.arg = arg
+    }
+
+    // post
+    static modepost() {
+        return new Promise((resolve, reject)=> {
+            instance.post(this.url, this.arg)
+            .then(res=>{
+                resolve(res)
+            })
+            .catch(err =>{
+                reject(err)
+            })
+        })
+    }
+
+    // get
+    static modeget() {
+        return new Promise((resolve, reject)=>{
+            instance.get(this.url)
+            .then(res=>{
+                resolve(res)
+            })
+            .catch(err=>{
+                reject(err)
+            })
+        })
+    }
+}
+
+export default request
