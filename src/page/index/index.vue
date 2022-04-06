7fcc9861a782863a04fb3c7f93614c0114678a04
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-07 00:57:34 +0800

    [part2] Complete the index js

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>111111</div>
+    <div>
+        <!-- 顶部 -->
+        <div class="sidebar-top">
+           <div>拳皇管理系统</div>
+           <div>退出</div>
+        </div>
+
+        <div class="sidebar-cont">1111
+
+        </div>
+
+    </div>
 </template>
 
 <script></script>
