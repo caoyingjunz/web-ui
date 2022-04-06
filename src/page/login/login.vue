ceacca35d4420b7e1ac861b255d74f3eaa0ec500
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-07 00:15:28 +0800

    set token in localstorage

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -14,8 +14,8 @@
             <!-- 登陆和注册切换 -->
             <div class="reg-view" @click="regx = regx== '登陆' ? '注册' : '登陆' "> <p> {{regx}} </p> </div>
             <!-- 登陆或者注册 -->
-            <el-button v-if="regx == '注册'"  @click="signin"  type="success" class="meituan-btn">登陆</el-button>
-            <el-button v-else type="success" @click="register" class="meituan-btn">注册</el-button>
+            <el-button v-if="regx == '注册'"  @click="signin"  type="success" :loading="load" class="meituan-btn">登陆</el-button>
+            <el-button v-else type="success" @click="register" :loading="load" class="meituan-btn">注册</el-button>
             </div>
         </div>
    </div>
@@ -29,26 +29,52 @@ export default {
         const user = reactive({
             account: '',
             password: '',
-            regx: '注册'
+            regx: '注册',
+            load: false
         })
 
         // 登陆
-        function signin() {
-            console.log("登陆",user.account, user.password)
+        const signin = async()=>{
+            user.load = true
+            const obj = {account: user.account, password: user.password}
+
+            try {
+                const res = await new proxy.$request(proxy.$urls.m().login, obj).doPost()
+                console.log(res)
+
+                if (res.status != 200) {
+                   new proxy.$tips(res.data.msg, "error").print_message()
+                } else {
+                    // 跳转到内容页面
+                    // 缓存 token 到本地
+                    localStorage.setItem("token", res.data.token)
+                 }
+
+            } catch (error) {
+                new proxy.$tips('内部服务器发生错误', "error").print_message()
+            }
+            user.load =false
         }
 
         // 注册
         const register = async()=> {
+            user.load = true
             const obj = {account: user.account, password: user.password}
-            const res = await new proxy.$request(proxy.$urls.m().register, obj).doPost()
-            // console.log(res)
 
-            if (res.status != 200) {
-                new proxy.$tips(res.data.msg, "error").print_message()
-            } else {
-                new proxy.$tips(res.data.msg).print_message()
-                user.regx = '注册'
+            try {
+                const res = await new proxy.$request(proxy.$urls.m().register, obj).doPost()
+                console.log(res)
+
+                if (res.status != 200) {
+                    new proxy.$tips(res.data.msg, "error").print_message()
+                } else {
+                    new proxy.$tips(res.data.msg).print_message()
+                    user.regx = '注册'
+                 }
+            } catch (error) {
+                new proxy.$tips('内部服务器发生错误', "error").print_message()
             }
+            user.load =false
         }
 
         return {...toRefs(user), signin, register}
