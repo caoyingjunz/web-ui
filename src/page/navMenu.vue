ea2a7eec01591df3bd151b0da9b1e36e6733e035
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-10 03:46:31 +0800

    reactor

--- src/page/navMenu.vue
+++ src/page/navMenu.vue
@@ -1,6 +1,12 @@
 <template>
+    <div class="sidebar-top">
+        <div>拳皇管理系统</div>
+        <div @click="logout">退出</div>
+    </div>
+
+    <div class="sidebar-cont">
+
     <el-aside width="200px">
-        <!-- <div class="sidebar-cont"> -->
         <el-menu
             default-active="1"
             class="el-menu-vertical-demo" router>
@@ -13,36 +19,49 @@
 
             <!-- 无子集 -->
             <el-menu-item index="2">
-                <el-icon><Notebook /></el-icon>
+                <el-icon><UserFilled /></el-icon>
                 <span>用户列表</span>
             </el-menu-item>
 
+            <!-- 无子集 -->
+            <el-menu-item index="3">
+                <el-icon><Notebook /></el-icon>
+                <span>订单管理</span>
+            </el-menu-item>
+
             <!-- 有子集 -->
-            <el-sub-menu index="3">
+            <el-sub-menu index="4">
             <template #title>
                 <el-icon><Setting /></el-icon>
                 <span>系统设置</span>
               </template>
               <el-menu-item-group>
-                <el-menu-item index="3-1">
-                    <el-icon><UserFilled /></el-icon>
+                <el-menu-item index="4-1">
+                    <el-icon><Lock /></el-icon>
                     <span>权限设置</span>
+                </el-menu-item>
+                <el-menu-item index="4-2">
+                    <el-icon><UserFilled /></el-icon>
+                    <span>角色设置</span>
                 </el-menu-item>
               </el-menu-item-group>
             </el-sub-menu>
-
         </el-menu>
-        <!-- </div> -->
     </el-aside>
+
+    </div>
 </template>
 
 <script>
+// import {getCurrentInstance} from 'vue'
+
 import {
     Histogram,
     Setting,
     Notebook,
     UserFilled,
-  } from '@element-plus/icons-vue'
+    Lock,
+} from '@element-plus/icons-vue'
 
 export default {
     data() {
@@ -51,12 +70,20 @@ export default {
         }
     },
     methods: {
-
+        // 退出登陆
+        logout(){
+            // const {proxy} = getCurrentInstance()
+            // 清除本地缓存的 token 和 menuid
+            localStorage.clear()
+            console.log('logout')
+            // proxy.$router.push('/ss')
+        }
     },
     components: {
         Histogram,
         Notebook,
         Setting,
+        Lock,
         UserFilled
     }
 }
