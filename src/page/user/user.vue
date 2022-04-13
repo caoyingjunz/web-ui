4b3b3772c2958693ed31b4d73d8e1dd6e27eea3a
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-14 00:43:17 +0800

    Add draggable

--- src/page/user/user.vue
+++ src/page/user/user.vue
@@ -146,9 +146,10 @@ data() {
             console.log(raw)
             ElMessageBox.confirm('此操作将永久删除该用户. 是否继续?','提示',
             {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning',
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning',
+                draggable: true,
             }
         )
             .then(() => {
