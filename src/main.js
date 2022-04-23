fc577f1efa790727f8a75b02c9b5c7db5ba410ca
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-24 00:35:08 +0800

    Optimise login code (#22)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import router from './router/index.js'
+import { ElMessage } from 'element-plus'
+
 
 // 全局引入css
 import '../style/headtap.css'
@@ -15,15 +17,44 @@ app.use(router)
 // 请求地址
 import urls from '../api/api.js'
 app.config.globalProperties.$urls = urls
+
 // 请求方法
 import request from '../api/request.js'
 app.config.globalProperties.$request = request
-// 弹窗tips
-import tips from '../config/tips.js'
-app.config.globalProperties.$tips = tips
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8080'
+
+// http拦截: 在 axios 请求发送之前给每个接口携带token去后端校验
+axios.interceptors.request.use(config => {
+    config.headers.Authorization = window.sessionStorage.getItem("token") // 服务端从请求头中获取 Authorization 作为 token
+    return config
+})
+// http拦截: 在 axios 请求发送之后, 最返回值进行处理
+axios.interceptors.response.use(
+    response => {
+        return response
+    },
+    error => {
+        if (error.response) {
+            let status = error.response.status
+            switch(status) {
+                case 401:
+                ElMessage({
+                    message: error.response.data,
+                    type: 'error',
+                })
+
+                window.sessionStorage.clear()
+                // 跳转到登陆界面
+                router.push("/")
+
+                break;
+            }
+        }
+        return Promise.reject("Unauthorized") // 返回错误信息
+    }
+)
 app.config.globalProperties.$http = axios
 
 app.mount('#app')
--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -5,78 +5,50 @@
             <div class="meituan-titile"> 拳皇管理系统 </div>
             <div class="meituan-user">
                 <p>账号</p>
-                <el-input v-model="account"  class="inputflex" placeholder="请输入账号" clearable />
+                <el-input v-model="user.account" class="inputflex" placeholder="请输入账号" clearable />
             </div>
             <div class="meituan-user">
                 <p>密码</p>
-                <el-input v-model="password" class="inputflex" placeholder="请输入密码" show-password/>
+                <el-input v-model="user.password" class="inputflex" placeholder="请输入密码" show-password/>
             </div>
-            <!-- 登陆和注册切换 -->
-            <div class="reg-view" @click="regx = regx== '登陆' ? '注册' : '登陆' "> <p> {{regx}} </p> </div>
-            <!-- 登陆或者注册 -->
-            <el-button v-if="regx == '注册'"  @click="signin"  type="success" :loading="load" class="meituan-btn">登陆</el-button>
-            <el-button v-else type="success" @click="register" :loading="load" class="meituan-btn">注册</el-button>
+
+            <div class="reg-view"> </div>
+            <el-button @click="login" type="success" :loading="load" class="meituan-btn">登陆</el-button>
+
             </div>
         </div>
    </div>
 </template>
 
 <script>
-import {reactive, toRefs, getCurrentInstance} from 'vue'
 export default {
-    setup() {
-        const {proxy} = getCurrentInstance()
-        const user = reactive({
-            account: '',
-            password: '',
-            regx: '注册',
+    data() {
+        return {
+            user: {
+                account: "",
+                password: "",
+            },
             load: false
-        })
-
-        // 登陆
-        const signin = async()=>{
-            user.load = true
-            const obj = {account: user.account, password: user.password}
-
-            try {
-                const res = await new proxy.$request(proxy.$urls.m().login, obj).doPost()
-                // console.log(res)
-                if (res.status != 200) {
-                   new proxy.$tips(res.data.msg, "error").print_message()
-                } else {
-                    // 跳转到内容页面
-                    localStorage.setItem("token", res.data.data.token) // 缓存 token 到本地
-                    // proxy.$router.push({name:'index'})
-                    proxy.$router.push('/index')
-                 }
-            } catch (error) {
-                console.log(error)
-                new proxy.$tips('内部服务器发生错误', "error").print_message()
-            }
-            user.load =false
         }
+    },
+    methods: {
+        async login(){
+            this.load = true
+            if (this.user.account == "admin" && this.user.password == "admin" ) {
+                this.load =false
+                this.$message.success("登陆成功")
+                // 缓存 token 到本地
+                window.sessionStorage.setItem("token", "faketoken")
 
-        // 注册
-        const register = async()=> {
-            user.load = true
-            const obj = {account: user.account, password: user.password}
-
-            try {
-                const res = await new proxy.$request(proxy.$urls.m().register, obj).doPost()
-                // console.log(res)
-                if (res.status != 200) {
-                    new proxy.$tips(res.data.msg, "error").print_message()
-                } else {
-                    new proxy.$tips(res.data.msg).print_message()
-                    user.regx = '注册'
-                 }
-            } catch (error) {
-                new proxy.$tips('内部服务器发生错误', "error").print_message()
+                this.$router.push('/index')
+            } else {
+                this.load =false
+                this.$message.error("用户名或密码错误")
             }
-            user.load =false
-        }
+        },
+    },
+    components: {
 
-        return {...toRefs(user), signin, register}
     }
 }
 </script>
