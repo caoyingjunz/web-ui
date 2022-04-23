437a92cf84175a24fa7e6562ff0b964e07d33e79
caoyingjunz <cao.yingjunz@gmail.com>
2022-04-23 22:27:50 +0800

    Optimise login code

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -11,13 +11,10 @@
                 <p>密码</p>
                 <el-input v-model="user.password" class="inputflex" placeholder="请输入密码" show-password/>
             </div>
-            <!-- 登陆和注册切换 -->
-            <!-- <div class="reg-view" @click="regx = regx== '登陆' ? '注册' : '登陆' "> <p> {{regx}} </p> </div> -->
-            <div class="reg-view" @click="登陆"> <p> </p> </div>
 
-            <!-- 登陆或者注册 -->
-            <el-button v-if="regx == '注册'"  @click="signin"  type="success" :loading="load" class="meituan-btn">登陆</el-button>
-            <!-- <el-button v-else type="success" @click="register" :loading="load" class="meituan-btn">注册</el-button> -->
+            <div class="reg-view"> </div>
+            <el-button @click="login"  type="success" :loading="load" class="meituan-btn">登陆</el-button>
+
             </div>
         </div>
    </div>
@@ -35,19 +32,20 @@ export default {
         }
     },
     methods: {
-        async signin(){
+        async login(){
             this.load = true
-
             if (this.user.account == "admin" && this.user.password == "admin" ) {
                 this.load =false
-                proxy.$router.push('/index')
-            } else{
+                this.$message.success("登陆成功")
+                this.$router.push('/index')
+            } else {
                 this.load =false
-                new proxy.$tips('用户名或密码错误', "error").print_message()
+                this.$message.error("用户名或密码错误")
             }
-          },
+        },
+    },
     components: {
-    }
+
     }
 }
 </script>
