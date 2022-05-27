19e4f3b0528fcd83cd4e6cc384242b8c129209b0
caoyingjunz <cao.yingjunz@gmail.com>
2022-05-28 00:17:18 +0800

    clean

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -8,7 +8,12 @@
         </div>
 
         <div style="display: flex; align-items:center;">
-          <el-button style="margin: 0px 20px;"  type="text" @click="logout"> 退出 </el-button>
+
+          <!-- <div class="block" style="margin: 30px 0px 10px;">
+            <el-avatar :size="40" :src="circleUrl"/>
+          </div> -->
+
+          <el-button style="margin: 0px 20px;" type="text" @click="logout"> 退出 </el-button>
         </div>
       </el-header>
 
@@ -90,12 +95,8 @@ import {
 export default {
     data() {
         return {
-          Histogram: '',
-          Notebook: '',
-          Setting: '',
-          Lock: '',
-          UserFilled: '',
           activeIndex: '',
+          circleUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
         }
     },
     created(){
