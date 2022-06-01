d2fb04e874c2685466f6d3932438a218d6cd0611
caoyingjunz <cao.yingjunz@gmail.com>
2022-06-01 23:38:02 +0800

    Add label detail page (#51)

--- src/page/book/labeldetail.vue
+++ src/page/book/labeldetail.vue
@@ -1,10 +1,44 @@
 <template>
-    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-        <el-tab-pane label="详情" name="first">User</el-tab-pane>
-        <el-tab-pane label="标签列表" name="second">Config</el-tab-pane>
-        <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-        <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
-    </el-tabs>
+    <div>
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/index' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/#/label">标签管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>标签详情</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>详情</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+
+          </el-card>
+
+        <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+            <el-tab-pane label="详情" name="first">
+
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                      <span>详细信息</span>
+                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                  </el-card>
+
+            </el-tab-pane>
+            <el-tab-pane label="标签列表" name="second">
+
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                      <span>标签列表</span>
+                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                  </el-card>
+
+            </el-tab-pane>
+            <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+            <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
 
 <script>
@@ -34,10 +68,33 @@ export default {
 </script>
 
 <style>
-.demo-tabs > .el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
+.demo-tabs>.el-tabs__content {
+    padding: 32px;
+    color: #6b778c;
+    font-size: 32px;
+    font-weight: 600;
 }
+
+.text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    color: #000000;
+    /* width: 480px; */
+  }
+
 </style>
