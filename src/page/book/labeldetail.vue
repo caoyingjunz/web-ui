b5bf8687a1d53503be3e97f2c11b3ca5499c81f8
caoyingjunz <cao.yingjunz@gmail.com>
2022-06-01 23:31:28 +0800

    Add src/page/book/labeldetail.vue

--- src/page/book/labeldetail.vue
+++ src/page/book/labeldetail.vue
@@ -6,9 +6,35 @@
             <el-breadcrumb-item>标签详情</el-breadcrumb-item>
         </el-breadcrumb>
 
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>详情</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+
+          </el-card>
+
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-            <el-tab-pane label="详情" name="first">User</el-tab-pane>
-            <el-tab-pane label="标签列表" name="second">Config</el-tab-pane>
+            <el-tab-pane label="详情" name="first">
+
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                      <span>详细信息</span>
+                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                  </el-card>
+
+            </el-tab-pane>
+            <el-tab-pane label="标签列表" name="second">
+
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                      <span>标签列表</span>
+                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                  </el-card>
+
+            </el-tab-pane>
             <el-tab-pane label="Role" name="third">Role</el-tab-pane>
             <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
         </el-tabs>
@@ -41,10 +67,33 @@
 </script>
 
 <style>
-    .demo-tabs>.el-tabs__content {
-        padding: 32px;
-        color: #6b778c;
-        font-size: 32px;
-        font-weight: 600;
-    }
+.demo-tabs>.el-tabs__content {
+    padding: 32px;
+    color: #6b778c;
+    font-size: 32px;
+    font-weight: 600;
+}
+
+.text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    color: #000000;
+    /* width: 480px; */
+  }
+
 </style>
