f27a465cae2b26f6ba68fa8c045f03f621d47e08
caoyingjunz <cao.yingjunz@gmail.com>
2022-06-02 00:20:25 +0800

    Fix activeName is null for created

--- src/page/book/labeldetail.vue
+++ src/page/book/labeldetail.vue
@@ -8,31 +8,31 @@
 
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-              <span>详情</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                <span>详情</span>
+                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
             </div>
 
-          </el-card>
+        </el-card>
 
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
             <el-tab-pane label="详情" name="first">
 
                 <el-card class="box-card">
                     <div slot="header" class="clearfix">
-                      <span>详细信息</span>
-                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                        <span>详细信息</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
                     </div>
-                  </el-card>
+                </el-card>
 
             </el-tab-pane>
             <el-tab-pane label="标签列表" name="second">
 
                 <el-card class="box-card">
                     <div slot="header" class="clearfix">
-                      <span>标签列表</span>
-                      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                        <span>标签列表</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
                     </div>
-                  </el-card>
+                </el-card>
 
             </el-tab-pane>
             <el-tab-pane label="Role" name="third">Role</el-tab-pane>
@@ -42,59 +42,62 @@
 </template>
 
 <script>
-export default {
-  data() {
-      return {
-          label_id: 0,
-          activeName: "first"
-      }
-  },
-  created() {
-      this.activeName = window.sessionStorage.getItem("activeName")
-      this.label_id = this.$route.params.label_id
-  },
-  methods: {
-      handleClick(tab, event) {
-          this.activeName = tab.props.name
-          window.sessionStorage.setItem("activeName", this.activeName)
-      },
-      handleOpen(){
-      }
-  },
-  components: {
+    export default {
+        data() {
+            return {
+                label_id: 0,
+                activeName: "first"
+            }
+        },
+        created() {
+            this.activeName = window.sessionStorage.getItem("activeName")
+            if (this.activeName == null) {
+                this.activeName = "first"
+            }
 
-  },
-}
+            this.label_id = this.$route.params.label_id
+        },
+        methods: {
+            handleClick(tab, event) {
+                this.activeName = tab.props.name
+                window.sessionStorage.setItem("activeName", this.activeName)
+            },
+            handleOpen() {}
+        },
+        components: {
+
+        },
+    }
 </script>
 
 <style>
-.demo-tabs>.el-tabs__content {
-    padding: 32px;
-    color: #6b778c;
-    font-size: 32px;
-    font-weight: 600;
-}
+    .demo-tabs>.el-tabs__content {
+        padding: 32px;
+        color: #6b778c;
+        font-size: 32px;
+        font-weight: 600;
+    }
 
-.text {
-    font-size: 14px;
-  }
+    .text {
+        font-size: 14px;
+    }
 
-  .item {
-    margin-bottom: 18px;
-  }
+    .item {
+        margin-bottom: 18px;
+    }
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
 
-  .box-card {
-    color: #000000;
-    /* width: 480px; */
-  }
+    .clearfix:after {
+        clear: both
+    }
 
+    .box-card {
+        color: #000000;
+        /* width: 480px; */
+    }
 </style>
