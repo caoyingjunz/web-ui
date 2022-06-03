75a5c96bb58a88711963f5d5ecd5f618f98ca2e8
caoyingjunz <cao.yingjunz@gmail.com>
2022-06-03 19:15:31 +0800

    Add labeldetail display

--- src/page/book/labeldetail.vue
+++ src/page/book/labeldetail.vue
@@ -17,11 +17,46 @@
 
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
             <el-tab-pane label="详细信息" name="first">
-               <div>
+                <div style="margin: 20px 20px 40px 0;">
 
+                    <el-form>
+                        <el-form :inline="true" :model="label_info">
+                            <el-form-item label="标签名称:">
+                                <el-input v-model="label_info.name" />
+                            </el-form-item>
 
+                            <el-form-item label="创建时间:">
+                                <el-input v-model="label_info.gmt_create" />
+                            </el-form-item>
 
-               </div>
+                            <el-form-item label="更新时间:">
+                                <el-input v-model="label_info.gmt_modified" />
+                            </el-form-item>
+                        </el-form>
+
+                        <el-form>
+                            <el-form-item label="标签值:">
+                            </el-form-item>
+                        </el-form>
+
+                        <el-form>
+                            <el-form-item>
+                                <el-tag hit :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false"
+                                    @close="handleClose(tag)">
+                                    {{ tag }}
+                                </el-tag>
+                                <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue"
+                                    ref="saveTagInput" size="default" @keyup.enter.native="handleInputConfirm"
+                                    @blur="handleInputConfirm">
+                                </el-input>
+                                <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag
+                                </el-button>
+
+                            </el-form-item>
+                        </el-form>
+                    </el-form>
+
+                </div>
 
             </el-tab-pane>
             <el-tab-pane label="标签列表" name="second">
@@ -33,8 +68,7 @@
                 </el-card>
 
             </el-tab-pane>
-            <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-            <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+            <el-tab-pane label="其他信息" name="third">Role</el-tab-pane>
         </el-tabs>
     </div>
 </template>
@@ -44,6 +78,9 @@
     export default {
         data() {
             return {
+                inputVisible: false,
+                inputValue: '',
+                dynamicTags: [],
                 label_info: {
                     "label_id": 0,
                     "resource_version": 0,
@@ -64,14 +101,31 @@
             }
 
             this.label_id = this.$route.params.label_id
-            // 如果已经切换到标签页面，刷新时，从缓存中获取标签id
-            if (this.label_id == undefined ){
+            // 如果已经切换到标签页面，刷新时，从缓存中获取标签 id
+            if (this.label_id == undefined) {
                 this.label_id = window.sessionStorage.getItem("activeLableId")
             }
 
             this.getLabelDetail(this.label_id)
         },
         methods: {
+            handleClose(tag) {
+                this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+            },
+            showInput() {
+                this.inputVisible = true;
+                this.$nextTick(_ => {
+                    this.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+            handleInputConfirm() {
+                let inputValue = this.inputValue;
+                if (inputValue) {
+                    this.dynamicTags.push(inputValue);
+                }
+                this.inputVisible = false;
+                this.inputValue = '';
+            },
             handleClick(tab, event) {
                 this.activeName = tab.props.name
                 window.sessionStorage.setItem("activeName", this.activeName)
@@ -87,6 +141,7 @@
                 }
 
                 this.label_info = res.result
+                this.dynamicTags = res.result.content.split(',')
             },
         },
         components: {
@@ -126,5 +181,23 @@
     .box-card {
         margin: 00px 0px 00px 0px;
         /* width: 480px; */
+    }
+
+    .el-tag+.el-tag {
+        margin-left: 6px;
+    }
+
+    .button-new-tag {
+        margin-left: 10px;
+        /* height: 32px; */
+        /* line-height: 30px; */
+        /* padding-top: 0;
+    padding-bottom: 0; */
+    }
+
+    .input-new-tag {
+        width: 120px;
+        margin-left: 10px;
+        vertical-align: bottom;
     }
 </style>
